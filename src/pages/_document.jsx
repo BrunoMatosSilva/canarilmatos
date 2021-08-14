b273b0791dc2fc99094ec795dbbf7e35c904bd71
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-08-14 14:33:20 -0300

    styled components config

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,6 +1,40 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document';
+
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx
+  ) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html>
@@ -9,6 +43,9 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
         <body>
           <Main />
